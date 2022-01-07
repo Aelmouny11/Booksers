9555841b2a9844af5865a1f1c470181b5a8c218b
aelmouny11 <azddine.elmoumny@edu.uca.ma>
2022-01-07 23:08:29 +0100

    dark Mode
    Setup Dark mode with next-themes and tailwindcss

--- pages/components/header.jsx
+++ pages/components/header.jsx
@@ -1,20 +1,48 @@
+import { useState,useEffect } from "react";
+import { useTheme } from "next-themes";
 import Link from "next/link";
+import { MoonIcon, SunIcon } from "@heroicons/react/solid";
 import Search from "./search";
 const links = ["Home", "Catgories", "About", "Support"];
-
 const Header = () => {
+  const {systemTheme, theme, setTheme } = useTheme();
+  const [mounted,setMounted]=useState(false);
+  useEffect(()=>{
+    setMounted(true)
+  },[])
+  const renderThemeChanger=()=>{
+    if(!mounted) return null;
+    const currentTheme=theme==='system'? systemTheme:theme;
+    if(currentTheme==='dark'){
+      return(
+        <SunIcon
+        className="w-7 h-7"
+        role="button"
+        onClick={()=>setTheme('light')}
+        />
+      );
+    }
+   else{
+    return(
+      <MoonIcon
+      className="w-7 h-7"    role="button" onClick={()=>setTheme('dark')}
+      />
+    );
+  }
+}
   return (
-    <div className="bg-indigo-700  pb-0 grid gap-5">
-      <div className="p-4">
+    <div className="bg-indigo-700  pb-0 grid gap-5 w-full dark:bg-slate-500  justify-between items-center">
+      <div className="p-4 ">
         {" "}
         <Link href="#">
           <h1 className="text-white font-bold">Booksers</h1>
         </Link>
+        {renderThemeChanger()}
       </div>
-      <div className="bg-teal-200 flex items-center gap-4 p-2 pl-10  w-full">
+      <div className="bg-teal-200 flex items-center gap-4 p-2 pl-10  w-full dark:bg-indigo-600">
         {links.map((ln) => {
           return (
-            <div>
+            <div className="">
               <Link href="#">{ln}</Link>
             </div>
           );
--- pages/components/search.jsx
+++ pages/components/search.jsx
@@ -3,7 +3,7 @@ const Search = () => {
     <>
       <form action="" className="w-full h-8 p-2">
           <input type="text" className="w-1/2 rounded-full pl-3 pt-1" placeholder="Search Here ..." />
-          <button className="bg-purple-500 ">Search</button>
+          <button className="bg-purple-500 w-auto h-6 px-4 justify-center items-center">search</button>
       </form>
     </>
   );
